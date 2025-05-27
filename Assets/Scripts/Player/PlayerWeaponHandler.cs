b7f6d564b6b22707c411f6eeaa7d42a736c8debf
Jangminez <111687963+Jangminez@users.noreply.github.com>
2025-05-28 00:31:39 +0900

    feat: 무기 관련 작업 중

--- Assets/Scripts/Player/PlayerWeaponHandler.cs
+++ Assets/Scripts/Player/PlayerWeaponHandler.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class PlayerWeaponHandler : MonoBehaviour
 {
     private Player player;
+    [SerializeField] WeaponController[] weapons;
+    [SerializeField] WeaponController curWeapon;
 
     public void Init(Player player)
     {
         this.player = player;
+    }
+
+    public void ChangeWeapon(int idx)
+    {
+        curWeapon = weapons[idx];
+        curWeapon.Init();
     }
 }
---
+++ Assets/Scripts/Weapon/ScriptableObject/WeaponDataSO.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public enum WeaponType { Buff, Debuff }
+
+[System.Serializable]
+public struct BuffEffect
+{
+    public float attackSpeedMultiplier; 
+}
+
+[System.Serializable]
+public struct DebuffEffect
+{
+    public float defenseReductionPercent;
+}
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "Weapon/New WeaponData")]
+public class WeaponDataSO : ScriptableObject
+{
+    [Header("Weapon Type")]
+    public WeaponType weaponType;
+
+    [Header("Weapon Stats")]
+    public float range;  
+    public float duration;    
+    public float cooldown;
+    public float useStamina;
+
+    [Header("Effect")]
+    public BuffEffect buffEffect;
+    public DebuffEffect debuffEffect;
+
+    [Header("Unlock")]
+    public string unlockCondition; 
+}
